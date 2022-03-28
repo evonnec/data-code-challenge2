98f9e48ab1581deb11d0b6f62706fb1a6f3a1ca3
Leevar Williams <leevar.williams@gmail.com>
2022-03-28 12:37:19 -0700

    Update connection.py with correct connection params

--- src/connection.py
+++ src/connection.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 
 import pandas as pd
-import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy.engine.base import Connection
 
@@ -9,23 +8,26 @@ from sqlalchemy.engine.base import Connection
 @dataclass
 class Settings:
     username: str = "appuser"
-    password: str = "app-password"
-    database_name: str = "formulary_db"
+    password: str = "appuser"
+    host: str = "localhost"
+    port: int = 5432
+    database_name: str = "formulary"
 
 
 def get_connection(settings: Settings) -> Connection:
     """Create a connection to the database."""
     alchemyEngine = create_engine(
-        f'postgresql+psycopg2://{settings.username}:{settings.password}@127.0.0.1/{settings.database_name}',
-        pool_recycle=3600
+        (
+            f"postgresql+psycopg2://{settings.username}:{settings.password}@"
+            f"{settings.host}:{settings.port}/{settings.database_name}"
+        ),
+        pool_recycle=3600,
     )
-    db_connection = alchemyEngine.connect();
+    db_connection = alchemyEngine.connect()
     return db_connection
-
 
 
 def get_test_data(db_connection: Connection) -> pd.DataFrame:
     """Test function to confirm access to the data in the database."""
     dataframe = pd.read_sql("select * from test_formulary limit 10;", db_connection)
     return dataframe
-
