de911aa7bf4069db3e9aaf007212b139688a8369
evonne-h1 <evonne.cho@h1insights.com>
2022-11-30 11:18:41 -0500

    Does not need formulary id's not equal to each other

--- src/solution.sql
+++ src/solution.sql
@@ -14,8 +14,6 @@ LEFT JOIN
 (SELECT formulary_id, rxcui, tier_level_value FROM test_formulary) b
 ON
 a.rxcui = b.rxcui
-WHERE
-a.formulary_id <> b.formulary_id
 
 --- table 3A:
 SELECT c.formulary_id AS formulary_id, 
@@ -31,8 +29,7 @@ FROM
 LEFT JOIN
 (SELECT formulary_id, rxcui, tier_level_value FROM test_formulary) b
 ON
-a.rxcui = b.rxcui 
-WHERE a.formulary_id <> b.formulary_id) c
+a.rxcui = b.rxcui) c
 GROUP BY c.formulary_id;
 
 --- table 3B:
@@ -51,14 +48,13 @@ LEFT JOIN
 ON
 a.rxcui = b.rxcui 
 WHERE
-a.formulary_id <> b.formulary_id
-AND
 a.tier_level_value - b.tier_level_value <> 0) c 
 GROUP BY c.formulary_id;
 
 -- Part 3:
 --- table 2:
---  Execution Time: 732.191 ms
+--  Execution Time: 719.343 ms
+
 EXPLAIN ANALYZE
 SELECT a.formulary_id AS formulary_target_id, 
 a.tier_level_value AS tier_level_value_target,
@@ -70,11 +66,9 @@ FROM
 INNER JOIN
 (SELECT formulary_id, rxcui, tier_level_value FROM formulary WHERE formulary_id = '00022170') b
 ON
-a.rxcui = b.rxcui
-WHERE b.formulary_id = '00022170'
-AND 
-b.formulary_id <> a.formulary_id;
---  Execution Time: 732.191 ms
+b.rxcui = a.rxcui
+WHERE b.formulary_id = '00022170';
+--  Execution Time: 719.343 ms
 --  formulary_target_id | tier_level_value_target |  rxcui  | formulary_id | tier_level_value 
 -- ---------------------+-------------------------+---------+--------------+------------------
 --  00022169            |                       3 | 1307421 | 00022170     |                3
@@ -124,29 +118,29 @@ b.formulary_id <> a.formulary_id;
 --  00022086            |                       1 | 1307421 | 00022170     |                3
 --                                                                    QUERY PLAN                                                                   
 -- ------------------------------------------------------------------------------------------------------------------------------------------------
---  Hash Join  (cost=62256.92..115871.38 rows=1171649 width=33) (actual time=477.769..690.523 rows=1168816 loops=1)
+--  Hash Join  (cost=62256.92..113151.98 rows=1191243 width=33) (actual time=410.662..583.181 rows=1171546 loops=1)
 --    Hash Cond: (formulary_1.rxcui = formulary.rxcui)
---    Join Filter: (formulary_1.formulary_id <> formulary.formulary_id)
---    Rows Removed by Join Filter: 2730
---    ->  Gather  (cost=1000.00..25934.70 rows=3375 width=20) (actual time=32.479..32.795 rows=2730 loops=1)
+--    ->  Gather  (cost=1000.00..25937.80 rows=3406 width=20) (actual time=44.496..44.814 rows=2730 loops=1)
 --          Workers Planned: 2
 --          Workers Launched: 2
---          ->  Parallel Seq Scan on formulary formulary_1  (cost=0.00..24597.20 rows=1406 width=20) (actual time=12.090..69.375 rows=910 loops=3)
+--          ->  Parallel Seq Scan on formulary formulary_1  (cost=0.00..24597.20 rows=1419 width=20) (actual time=22.544..49.173 rows=910 loops=3)
 --                Filter: (formulary_id = '00022170'::text)
 --                Rows Removed by Filter: 526911
---    ->  Hash  (cost=32184.63..32184.63 rows=1583463 width=20) (actual time=442.372..442.373 rows=1583463 loops=1)
+--    ->  Hash  (cost=32184.63..32184.63 rows=1583463 width=20) (actual time=365.612..365.612 rows=1583463 loops=1)
 --          Buckets: 131072  Batches: 16  Memory Usage: 6610kB
---          ->  Seq Scan on formulary  (cost=0.00..32184.63 rows=1583463 width=20) (actual time=0.017..179.450 rows=1583463 loops=1)
---  Planning Time: 1.590 ms
+--          ->  Seq Scan on formulary  (cost=0.00..32184.63 rows=1583463 width=20) (actual time=0.005..156.495 rows=1583463 loops=1)
+--  Planning Time: 0.573 ms
 --  JIT:
---    Functions: 22
+--    Functions: 20
 --    Options: Inlining false, Optimization false, Expressions true, Deforming true
---    Timing: Generation 13.238 ms, Inlining 0.000 ms, Optimization 2.981 ms, Emission 19.912 ms, Total 36.131 ms
---  Execution Time: 732.191 ms
+--    Timing: Generation 1.255 ms, Inlining 0.000 ms, Optimization 6.051 ms, Emission 18.740 ms, Total 26.046 ms
+--  Execution Time: 719.343 ms
+-- (17 rows)
 
 
 --- table 3A:
---  Execution Time: 501.229 ms
+--  Execution Time: 487.861 ms
+
 EXPLAIN ANALYZE
 SELECT c.formulary_id AS formulary_id, 
 avg(c.tier_level_value_target - c.tier_level_value) AS tier_difference 
@@ -161,41 +155,39 @@ from
 INNER JOIN
 (SELECT formulary_id, rxcui, tier_level_value FROM formulary WHERE formulary_id = '00022170') b
 ON
-a.rxcui = b.rxcui
-WHERE 
-a.formulary_id <> b.formulary_id) c
+b.rxcui = a.rxcui) c
 GROUP BY c.formulary_id;
 
 --  formulary_id |     tier_difference     
 -- --------------+-------------------------
 --  00022170     | -0.86675490410808886942
 --
---                                                                            QUERY PLAN                                                                           
--- ----------------------------------------------------------------------------------------------------------------------------------------------------------------
---  Finalize GroupAggregate  (cost=36060.96..72674.15 rows=458 width=41) (actual time=470.000..500.899 rows=1 loops=1)
+--                                                                          QUERY PLAN                                                                         
+-- ------------------------------------------------------------------------------------------------------------------------------------------------------------
+--  Finalize GroupAggregate  (cost=35416.96..69974.19 rows=458 width=41) (actual time=458.925..487.633 rows=1 loops=1)
 --    Group Key: formulary_1.formulary_id
---    ->  Gather  (cost=36060.96..72663.84 rows=916 width=41) (actual time=464.251..500.880 rows=3 loops=1)
+--    ->  Gather  (cost=35416.96..69963.89 rows=916 width=41) (actual time=456.148..487.603 rows=3 loops=1)
 --          Workers Planned: 2
 --          Workers Launched: 2
---          ->  Partial GroupAggregate  (cost=35060.96..71572.24 rows=458 width=41) (actual time=412.362..412.691 rows=1 loops=3)
+--          ->  Partial GroupAggregate  (cost=34416.96..68872.29 rows=458 width=41) (actual time=444.930..445.100 rows=1 loops=3)
 --                Group Key: formulary_1.formulary_id
---                ->  Parallel Hash Join  (cost=35060.96..67924.57 rows=485746 width=17) (actual time=277.908..389.151 rows=211889 loops=3)
+--                ->  Parallel Hash Join  (cost=34416.96..65145.08 rows=496351 width=17) (actual time=288.945..408.546 rows=390515 loops=3)
 --                      Hash Cond: (formulary_1.rxcui = formulary.rxcui)
---                      Join Filter: ((formulary_1.formulary_id <> formulary.formulary_id) AND ((formulary.tier_level_value - formulary_1.tier_level_value) <> 0))
---                      Rows Removed by Join Filter: 178626
---                      ->  Parallel Seq Scan on formulary formulary_1  (cost=0.00..24597.20 rows=1406 width=20) (actual time=16.868..36.390 rows=910 loops=3)
+--                      ->  Parallel Seq Scan on formulary formulary_1  (cost=0.00..24597.20 rows=1419 width=20) (actual time=11.166..35.608 rows=910 loops=3)
 --                            Filter: (formulary_id = '00022170'::text)
 --                            Rows Removed by Filter: 526911
---                      ->  Parallel Hash  (cost=22947.76..22947.76 rows=659776 width=20) (actual time=227.706..227.706 rows=527821 loops=3)
---                            Buckets: 131072  Batches: 16  Memory Usage: 7104kB
---                            ->  Parallel Seq Scan on formulary  (cost=0.00..22947.76 rows=659776 width=20) (actual time=0.037..92.209 rows=527821 loops=3)
---  Planning Time: 0.962 ms
---  Execution Time: 501.229 ms
+--                      ->  Parallel Hash  (cost=22947.76..22947.76 rows=659776 width=11) (actual time=233.229..233.229 rows=527821 loops=3)
+--                            Buckets: 262144  Batches: 16  Memory Usage: 7392kB
+--                            ->  Parallel Seq Scan on formulary  (cost=0.00..22947.76 rows=659776 width=11) (actual time=0.165..132.981 rows=527821 loops=3)
+--  Planning Time: 0.798 ms
+--  Execution Time: 487.861 ms
+-- (17 rows)
 
 
 
 --- table 3B:
--- Execution Time: 592.743 ms
+--  Execution Time: 373.323 ms
+
 EXPLAIN ANALYZE
 SELECT c.formulary_id AS formulary_id, 
 avg(c.tier_level_value_target - c.tier_level_value) AS tier_difference 
@@ -210,35 +202,33 @@ FROM
 INNER JOIN
 (select formulary_id, rxcui, tier_level_value FROM formulary WHERE formulary_id = '00022170') b
 ON
-a.rxcui = b.rxcui
+b.rxcui = a.rxcui) c 
 WHERE
-b.formulary_id <> a.formulary_id
-AND
-a.tier_level_value - b.tier_level_value <> 0) c 
+c.tier_level_value_target - c.tier_level_value <> 0
 GROUP BY c.formulary_id;
 
 --  formulary_id |   tier_difference   
 -- --------------+---------------------
 --  00022170     | -1.5937203068268341
---                                                                            QUERY PLAN                                                                           
--- ----------------------------------------------------------------------------------------------------------------------------------------------------------------
---  Finalize GroupAggregate  (cost=36060.96..72674.15 rows=458 width=41) (actual time=560.316..592.169 rows=1 loops=1)
+--                                                                          QUERY PLAN                                                                         
+-- ------------------------------------------------------------------------------------------------------------------------------------------------------------
+--  Finalize GroupAggregate  (cost=35416.96..70989.53 rows=458 width=41) (actual time=356.832..373.174 rows=1 loops=1)
 --    Group Key: formulary_1.formulary_id
---    ->  Gather  (cost=36060.96..72663.84 rows=916 width=41) (actual time=560.281..592.137 rows=3 loops=1)
+--    ->  Gather  (cost=35416.96..70979.23 rows=916 width=41) (actual time=353.142..373.166 rows=3 loops=1)
 --          Workers Planned: 2
 --          Workers Launched: 2
---          ->  Partial GroupAggregate  (cost=35060.96..71572.24 rows=458 width=41) (actual time=487.175..487.177 rows=1 loops=3)
+--          ->  Partial GroupAggregate  (cost=34416.96..69887.63 rows=458 width=41) (actual time=349.759..349.940 rows=1 loops=3)
 --                Group Key: formulary_1.formulary_id
---                ->  Parallel Hash Join  (cost=35060.96..67924.57 rows=485746 width=17) (actual time=342.238..469.578 rows=211889 loops=3)
+--                ->  Parallel Hash Join  (cost=34416.96..66179.02 rows=493870 width=17) (actual time=257.942..331.563 rows=211889 loops=3)
 --                      Hash Cond: (formulary_1.rxcui = formulary.rxcui)
---                      Join Filter: ((formulary_1.formulary_id <> formulary.formulary_id) AND ((formulary.tier_level_value - formulary_1.tier_level_value) <> 0))
+--                      Join Filter: ((formulary.tier_level_value - formulary_1.tier_level_value) <> 0)
 --                      Rows Removed by Join Filter: 178626
---                      ->  Parallel Seq Scan on formulary formulary_1  (cost=0.00..24597.20 rows=1406 width=20) (actual time=18.008..49.977 rows=910 loops=3)
+--                      ->  Parallel Seq Scan on formulary formulary_1  (cost=0.00..24597.20 rows=1419 width=20) (actual time=19.861..37.825 rows=910 loops=3)
 --                            Filter: (formulary_id = '00022170'::text)
 --                            Rows Removed by Filter: 526911
---                      ->  Parallel Hash  (cost=22947.76..22947.76 rows=659776 width=20) (actual time=270.026..270.026 rows=527821 loops=3)
---                            Buckets: 131072  Batches: 16  Memory Usage: 7104kB
---                            ->  Parallel Seq Scan on formulary  (cost=0.00..22947.76 rows=659776 width=20) (actual time=0.071..152.287 rows=527821 loops=3)
---  Planning Time: 1.748 ms
---  Execution Time: 592.743 ms
+--                      ->  Parallel Hash  (cost=22947.76..22947.76 rows=659776 width=11) (actual time=211.219..211.220 rows=527821 loops=3)
+--                            Buckets: 262144  Batches: 16  Memory Usage: 7392kB
+--                            ->  Parallel Seq Scan on formulary  (cost=0.00..22947.76 rows=659776 width=11) (actual time=0.086..93.264 rows=527821 loops=3)
+--  Planning Time: 0.386 ms
+--  Execution Time: 373.323 ms
 -- (19 rows)
